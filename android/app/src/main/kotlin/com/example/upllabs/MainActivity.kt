f0c2ef4c54db961beabbf4408d1e18652ecbda9d
Precious153 <adeleyeprecious16@gmail.com>
2022-08-10 07:26:39 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/upllabs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.upllabs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/onboardingScreen.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:upllabs/homepage.dart';
+import 'package:upllabs/sliderContent.dart';
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  int selectedSlider = 0;
+
+  PageController _pageController = PageController();
+
+  //Slider
+  List<Map<String, String>> sliders = [
+    {
+      "image": "images/on1.jpeg",
+      "title": "Perfect Healthcare \n Solution",
+      "text": "Find a specialist that you only\n need an consulting application"
+    },
+    {
+      "image": "images/on2.jpeg",
+      "title": "Perfect Healthcare \n Solution",
+      "text": "Find a specialist that you only\n need an consulting application"
+    },
+    {
+      "image": "images/on3.jpeg",
+      "title": "Perfect Healthcare \n Solution",
+      "text": "Find a specialist that you only\n need an consulting application"
+    }
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 5,
+                child: PageView.builder(
+                  onPageChanged: (index){
+                    setState(() {
+                      selectedSlider = index;
+                    });
+                  },
+                  controller: _pageController,
+                  itemCount: sliders.length,
+                  itemBuilder: (context, index) =>
+                      SliderContent(
+                        image: sliders[index]['image'] as String,
+                        title: sliders[index]['title'] as String,
+                        text: sliders[index]['text'] as String,),
+                ),
+              ),
+              SizedBox(height: 30),
+              Expanded(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                          sliders.length, (index) => buildSliderNav(index: index)),
+                    ),
+                  ],
+
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(0),
+                child:selectedSlider == 2 ?
+
+                OnboardButton() : null,
+              ),
+            ],
+          ),
+
+        ),
+      ),
+    );
+
+  }
+
+
+  AnimatedContainer buildSliderNav({required int index}) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 200),
+      margin: EdgeInsets.only(right: 5),
+      height: 5,
+      width: 15,
+      decoration: BoxDecoration(
+          color: selectedSlider == index ? Color(0xff363cc0) : Color.fromRGBO(230, 230, 230, 1),
+          borderRadius: BorderRadius.circular(5)
+      ),
+    );
+  }
+}
+class OnboardButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, right: 20, bottom: 40),
+      child: ElevatedButton(
+          onPressed:(){
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
+          },
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)
+            ),
+            minimumSize:Size(double.infinity, 50),
+            primary: Color(0xff000000),
+            elevation: 5,
+          ),
+          child: Text(
+            'Get Started',
+            style: TextStyle(
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+            ),
+          )
+      ),
+    );
+  }
+
+}
+
